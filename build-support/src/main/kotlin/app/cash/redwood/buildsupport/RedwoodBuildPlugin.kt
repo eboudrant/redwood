f3e395ba9d89922c19e86c1ef6f4e6aca5e8d7fd
Emmanuel Boudrant <eboudrant@netflix.com>
2026-03-24 17:29:10 -0700

    flag a version

--- build-support/src/main/kotlin/app/cash/redwood/buildsupport/RedwoodBuildPlugin.kt
+++ build-support/src/main/kotlin/app/cash/redwood/buildsupport/RedwoodBuildPlugin.kt
@@ -75,7 +75,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 private const val REDWOOD_GROUP_ID = "app.cash.redwood"
 
 // HEY! If you change the major version update release.yaml doc folder.
-private const val REDWOOD_VERSION = "0.20.0-SNAPSHOT"
+private const val REDWOOD_VERSION = "0.20.0-NETFLIX.1" // STOPSHIP: revert to upstream version before merging
 
 private val isCiEnvironment = System.getenv("CI") == "true"
 
